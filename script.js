c83d359be39f64f878f53688ed6b5d4745894027
Mohammed Zeeshan <64826609+FameZeeshan@users.noreply.github.com>
2023-03-06 09:20:49 +0530

    questions

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
+// Logical program questions based on conditional statements , logical operators and looping:
+
+// 1.	Write a program that checks if a number is positive, negative, or zero, and logs the result to the console using conditional operators.
+
+// 2.	Write a program that takes two numbers as input and checks if they are both even or both odd, and logs the result to the console using logical operators(using ternary operator).
+
+// 3.	Write a program that takes a year as input and checks if it's a leap year or not, and logs the result to the console using conditional and logical operators.
+
+// 4.	write a program to find the largest of three numbers?
+
+// 5.	Write a program that takes two numbers as input and determines if their sum is even.
+
+// 6.	Write a program that takes a number as input and determines if it is a multiple of 3 and 5.
+
+// 7.	Write a program that takes three numbers as input and determines if they are all odd or all even(using ternary operator).
+
+// 8.	Write a program that takes a number as input and returns "Skill" if it is a multiple of 3, "Safari" if it is a multiple of 5, and "Skill safari" if it is a multiple of both 3 and 5.
+
+// 9.	Write a program that takes three numbers as input and determines if they form a Pythagorean triplet.
+
+// 10.	Write a program that takes two integers as input and outputs whether the two integers have the same sign using a conditional statement and logical operators.
+
+// 11.	Write a program that takes three integers as input and outputs whether the three integers can form a valid triangle using a conditional statement and logical operators.
+// 12.	Write a program that takes two numbers as input and returns their greatest common divisor (GCD).
+
+// 13.	Write a program that takes a number as input and returns a boolean indicating if it is a perfect square. 
+
+// 14.	Write a program that takes two numbers as input and returns their greatest common divisor (GCD).
+
+// 15.	Write a program to find power of number using given number and power( 43).
+
+// 16.	Write a program that calculates the factorial of a non-negative integer.
+
+// 17.	Write a program that calculates the Fibonacci sequence up to a specified number of terms.
+
+// 18.	Write a program that checks if a given integer is prime
+
+// 19.	Write a program in JavaScript that uses a for loop to iterate over the given input and print "Skill" for numbers that are divisible by 3, "Safari" for numbers that are divisible by 5, and "Skill safari" for numbers that are divisible by both 3 and 5.
+
+// 20.	Write a program in JavaScript that uses nested for loops to print out a multiplication table for the numbers from 1 to 3 from that print 
+
+// 21.	Write a JavaScript program to construct the following pattern using nested for loop.
+
+// 22.	Write a Program to count the number of digits in 2023.
+
+// 23.	Write a Program to check if a number is a palindrome
+
+// 24.	Write a program that takes a string and returns a new string with all the vowels removed.
+
+// 25.	Write a program that takes a string and returns the longest word in the string.
+// 26.	Write a program to reverse a sentence word by word.
+
+// 27.	Write a programme that convert each word's first letter to uppercase.
+
+// 28.	Write a javascript program to construct the following number pattern using nested for loop.
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+// 16 17 18 19
+// 20 21 22
+// 23 24
+// 25
+
+// 29.	Write a Program to determine the day of the week based on its number(using switch statement):
+
+// 30.	Write a javascript program to construct the following number pattern using nested for loop.
+
+// 1
+// 3 2
+// 6 5 4
+// 10 9 8 7
+// 15 14 13 12 11
 
